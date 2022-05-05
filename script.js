b8e017de370c6a361c70c1bbb18c4dc1f431aff1
MadNake <72208527+MadNake@users.noreply.github.com>
2022-05-05 12:12:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 "use string"
 let letter = document.querySelectorAll('.symbol');
-let str = prompt("Что показать?","")
+let str =
+setTimeout(function() {
+str = prompt("Что показать?","")}, 2000);
+
 
 
 let conf = function() {confirm("Начинаем?") ? setTimeout(start, [1000]) : undefined;}
 console.log(letter);
 
-
 let start = function() {
   for (var i = 0; i < letter.length; i++) {
-    letter[i].textContent = 'b'
+    letter[i].classList.add("symbol-ani");
+  };
+  for (var i = (letter.length / 2) - (str.length / 2), j = 0; i < (letter.length / 2) + (str.length / 2); i++, j++ ) {
+      letter[i].textContent = str[j];
+      letter[i].classList.add("color");
   }
+  for (var i = 0; i < (letter.length / 2) - (str.length / 2); i++) {
+    letter[i].textContent = "$"
+  }
+  for (var i = (letter.length / 2) + (str.length / 2); i < letter.length; i++) {
+    letter[i].textContent = "%"
+  }
+
 }
 
 
-setTimeout(conf, [2000]);
+setTimeout(conf, 2000);
